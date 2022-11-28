b90b01d21df4803dece893728560559026a46433
Akhil <sai26091999@gmail.com>
2022-11-28 08:57:10 +0530

    Initial Commit

---
+++ FootBallLeagueMVC/FootBallLeagueMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using FootBallLeagueMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootBallLeagueMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        Database1 db=new Database1();
+
+        [HttpGet]
+        public ActionResult AddMatchResult()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMatchResult(FbLeague fbl)
+        {
+            db.FbLeagues.Add(fbl);
+            db.SaveChanges();
+            return RedirectToAction("Index1");
+           
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            List<FbLeague> list = db.FbLeagues.ToList();
+
+            return View(list);
+        }
+
+
+    }
+}
